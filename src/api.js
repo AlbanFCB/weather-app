a623618c44a54359a8d37b2777afc2ac45ae08ce
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-03-13 17:44:33 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      'X-RapidAPI-Key': 'f42eae1949msh9ea59477b7c0cc7p18c328jsn410402595929',
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+  export const WEATHER_API_KEY = "2cf46a77506ffbb0a16f810237a33756";
+  
